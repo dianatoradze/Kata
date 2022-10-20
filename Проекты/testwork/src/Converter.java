ad0564f1f32d6b18c12ac9c181ecadcef0d8826d
dianatoradze <dianatoradze1706@gmail.com>
2022-10-21 00:48:08 +0500

    init

---
+++ Проекты/testwork/src/Converter.java
@@ -0,0 +1,56 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Converter {
+    public static String arabicToRoman(int number) {
+        if ((number <= 0) || (number > 4000)) {
+            throw new IllegalArgumentException(number + " is not in range (0,4000]");
+        }
+
+        List romanNumerals = Numbers.getReverseSortedValues();
+
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while ((number > 0) && (i < romanNumerals.size())) {
+            Numbers currentSymbol = (Numbers) romanNumerals.get(i);
+            if (currentSymbol.getValue() <= number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else {
+                i++;
+            }
+        }
+
+        return sb.toString();
+    }
+    public static int romanToArabic(String input) {
+        String romanNumeral = input.toUpperCase();
+        int result = 0;
+
+        List romanNumerals = Numbers.getReverseSortedValues();
+
+        int i = 0;
+
+        while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+            Numbers symbol = (Numbers) romanNumerals.get(i);
+            if (romanNumeral.startsWith(symbol.name())) {
+                result += symbol.getValue();
+                romanNumeral = romanNumeral.substring(symbol.name().length());
+            } else {
+                i++;
+            }
+        }
+
+        if (romanNumeral.length() > 0) {
+            throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+        }
+
+        return result;
+    }
+    public boolean isRoman(String number){
+        //"V"->'V'
+        return Numbers.getReverseSortedValues().containsAll(Collections.singleton(number.charAt(0)));
+    }
+
+}
